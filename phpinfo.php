273fa1faaa4aa14fc196f392d8e570954e3fd50c
AndrwMan <anman@ucsd.edu>
2023-10-05 01:01:24 -0700

    closes #1, closes #2: modified httpd.conf, copied php8apache2_4.dll

--- phpinfo.php
+++ phpinfo.php
@@ -7,6 +7,14 @@
 	<?php 
 	phpinfo(); 
 	error_reporting(E_ALL);
+
+
+	// Check if OpenSSL is enabled in PHP
+	if (extension_loaded('openssl')) {
+        echo "OpenSSL is enabled in PHP.";
+    } else {
+        echo "OpenSSL is not enabled in PHP.";
+    }
 	?>
 </body>
 </html>
