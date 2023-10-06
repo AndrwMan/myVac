73ca90bc3712f78cedefe1b58ea976b70ff105fc
AndrwMan <anman@ucsd.edu>
2023-10-06 03:34:03 -0700

    added SPX closing prices graph over time, pos tracing bar

--- js/script.js
+++ js/script.js
@@ -6,6 +6,93 @@ document.addEventListener("DOMContentLoaded", function () {
 		console.log('D3.js is not loaded in this project.');
 	}
 
-	d3.select("body").append("p").text("Hello, D3!");
+	//d3.select("body").append("p").text("Hello, D3!");
 	console.log(spxData);
+
+	// Define dimensions of the graph
+	var width = 800;
+	var height = 400;
+	var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+
+	// Create an SVG container for the graph
+	var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+	// Set parse date function
+	var parseDate = d3.timeParse("%Y-%m-%d");
+
+	// Set the ranges for x and y
+	var x = d3.scaleTime().range([margin.left, width - margin.right]);
+	var y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+
+	// Define the line
+	var line = d3.line()
+	.x(function (d) { return x(d.date); })
+	.y(function (d) { return y(d.close); });
+
+	// Format the data
+	spxData.forEach(function (d) {
+	d.date = parseDate(d.date);
+	d.close = +d.close;
+	});
+
+	// Scale the range of the data
+	x.domain(d3.extent(spxData, function (d) { return d.date; }));
+	y.domain([0, d3.max(spxData, function (d) { return d.close; })]);
+
+	// Add the X-axis
+	svg.append("g")
+	.attr("transform", "translate(0," + (height - margin.bottom) + ")")
+	.call(d3.axisBottom(x));
+
+	// Add the Y-axis
+	svg.append("g")
+	.attr("transform", "translate(" + margin.left + ",0)")
+	.call(d3.axisLeft(y));
+
+	// Add the line
+	svg.append("path")
+	.datum(spxData)
+	.attr("fill", "none")
+	.attr("stroke", "steelblue")
+	.attr("stroke-width", 2)
+	.attr("d", line);
+
+	// Add interactive vertical bar
+	var verticalBar = svg.append("line")
+	.attr("x1", 0)
+	.attr("x2", 0)
+	.attr("y1", margin.top)
+	.attr("y2", height - margin.bottom)
+	.attr("stroke", "red")
+	.attr("stroke-width", 2)
+	.style("display", "none"); // Initially hidden
+
+	//Add mousemove event to show/hide vertical bar and update data
+	console.log(svg.node())
+	// do not use d3.mouse which was removed in d3 v6 onwards
+	// see: https://observablehq.com/@d3/d3v6-migration-guide#pointer
+	svg.on("mousemove", (event) => {
+		var pointer = d3.pointer(event);
+		var mouseX = pointer[0];			// get x-coordinate
+		var dateValue = x.invert(mouseX);	// converts pixel position to date value using x-scale
+		// find the closest data value in spxData array,
+		//  recall that user is free to point anywhere 
+		//  but not all dates are trading days and 
+		//  therefore may not have observed vals
+		var bisectDate = d3.bisector(function (d) { return d.date; }).left;
+		var index = bisectDate(spxData, dateValue, 1);
+		var dataPoint = spxData[index];
+	
+		if (dataPoint) {
+			// change the properties of the vertical bar on interaction 
+			//  shifts & adds visibility
+			verticalBar.attr("x1", mouseX).attr("x2", mouseX).style("display", "block");
+		} else {
+			verticalBar.style("display", "none");
+		}
+	});
+	
+
 });
