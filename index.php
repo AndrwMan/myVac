26e6fa89c45a0ba704b470a270cf5606a453aa67
AndrwMan <anman@ucsd.edu>
2023-10-05 21:40:03 -0700

    added yahoo finance fetch code, successfully fetched AAPL as test url

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <h1>Market Yield Data</h1>
 
     <?php
-    // FRED data fetching
+    /* fetch bonds data */
     // Fetch yield data for bonds w/ 3-months to 30 years maturity horizons 
 	// Map selected series_ids to series_names 
 	$series_ids = array(
@@ -37,7 +37,7 @@
 	$base_url = "https://api.stlouisfed.org/fred/series/observations";
 	$api_key = urlencode('fc29dfd642dbcd4d5c2d996a6e018b24'); //generated FRED apiKey
 	
-	//$frequency = urlencode("d");  // Set the frequency to daily
+	//$frequency = urlencode("d");  // Set the frequency to daily, do not need for right series
 
 	$file_type = urlencode("json");
 	//$file_type = urlencode("xml");	//uncomment respective code for returned format
@@ -45,26 +45,27 @@
 	// equivalent to `for series_id, series_name in series_ids.items():`
 	//  in Python, getting key and value pairs from iterable
 	foreach ($series_ids as $series_id => $series_name) {
-		$url = "{$base_url}?series_id={$series_id}&api_key={$api_key}&file_type={$file_type}";
-		//$url = "{$base_url}?series_id={$series_id}&api_key={$api_key}&observation_start={$start_date}&observation_end={$end_date}&file_type={$file_type}";
+		//$url = "{$base_url}?series_id={$series_id}&api_key={$api_key}&file_type={$file_type}";
+		$url = "{$base_url}?series_id={$series_id}&api_key={$api_key}&observation_start={$start_date}&observation_end={$end_date}&file_type={$file_type}";
 		//$url = "{$base_url}?series_id={$series_id}&api_key={$api_key}&observation_start={$start_date}&observation_end={$end_date}&file_type={$file_type}&frequency={$frequency}";
 		//echo $url; 
+
 		// Make the API request
 		$response = file_get_contents($url);
-		echo "Received API Response: <pre>";
-		print_r($response);
-		echo "</pre>";
+		// echo "Received API Response: <pre>";
+		// print_r($response);
+		// echo "</pre>";
 	
 		if ($response) {
 			$data = json_decode($response, true);
 
 			// debug: Use <pre> tags to format the output
-			echo "<pre>"; 
-			print_r($data);
-			echo "</pre>";
+			// echo "<pre>"; 
+			// print_r($data);
+			// echo "</pre>";
 				
 			// display the data for each series
-			echo "Market Yield Data for $series_name:\n";
+			echo "Market Yield Data for $series_name:<br>";
 			foreach ($data['observations'] as $observation) {
 				$date = $observation['date'];
 				$value = $observation['value'];
@@ -107,6 +108,70 @@
 			// }	
 		}
 	}
+
+
+	/* fetch S&P500 data*/
+	$symbol = '^SPX';
+
+    // Define the Yahoo Finance API URL for historical data
+    $base_url = "https://query1.finance.yahoo.com/v8/finance/chart/{$symbol}";
+	$test_url = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?metrics=high?&interval=1d&range=5d";
+	$test_url2 = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?";
+    
+    // Set relevant data range (from 1999-01-01 to today)
+    $start_date_tstamp = strtotime('1999-01-01'); // January 1, 1999
+	$end_date_tstamp = strtotime(date('Y-m-d')); // Today's date
+    
+    // Build the URL with appropriate parameters
+    //$url = "{$base_url}?period1=" . strtotime($start_date) . "&period2=" . strtotime($end_date) . "&interval=1d";
+	//$url = "{$base_url}?period1={$start_date_tstamp}&period2={$end_date_tstamp}&interval=1d";
+
+    $ch = curl_init($test_url2); 							// Initialize cURL session 
+
+	// Set cURL options
+	// Set the path to the CA certificate bundle (downloaded cacert.pem)
+	//  worked for absolute path vs relative path (to project root)
+	//curl_setopt($ch, CURLOPT_CAINFO, 'C:/Program Files (x86)/Apache24/htdocs/myVac/cacert.pem');
+	// Enable SSL certificate verification
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);						// Execute cURL session/fetch data
+
+	//debug: cURL response
+	echo "API Response:<pre>" . htmlspecialchars($response) . "</pre>";
+	if(curl_errno($ch)){
+		echo 'Curl error: ' . curl_error($ch);
+	}
+
+    curl_close($ch);		// Close cURL session
+
+    $data = json_decode($response, true);	
+	echo "<pre>"; 
+	print_r($data);
+	echo "</pre>";
+    // Check if data was successfully fetched
+    if ($data && isset($data['chart']['result'][0]['indicators']['quote'][0]['close'])) {
+        // Extract and display the daily data
+        $close_prices = $data['chart']['result'][0]['indicators']['quote'][0]['close'];
+        $timestamps = $data['chart']['result'][0]['timestamp'];
+        
+        echo "<h2>Daily Closing Prices for S&P 500 ($symbol)</h2>";
+        echo "<table>";
+        echo "<tr><th>Date</th><th>Close Price</th></tr>";
+        
+        foreach ($timestamps as $key => $timestamp) {
+            $date = date('Y-m-d', $timestamp);
+            $close_price = $close_prices[$key];
+            echo "<tr><td>{$date}</td><td>{$close_price}</td></tr>";
+        }
+        
+        echo "</table>";
+    } else {
+        echo "Failed to fetch data for S&P 500 ($symbol)";
+    }
     ?>
 </body>
 </html>
